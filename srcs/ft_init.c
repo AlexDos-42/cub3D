b28ca1b400b99c6171285ba01df9fe0fc88e55a6
Alex 42 <54394628+AlexDos-42@users.noreply.github.com>
2020-01-15 03:56:03 +0100

    Update ft_init.c

--- srcs/ft_init.c
+++ srcs/ft_init.c
@@ -4,25 +4,25 @@ void	initmlx(t_all *all)
 {
 	if (!(all->mlx.ptr = mlx_init()))
 	{
-		printf("Error\nmlx_init");
+		ft_printf("Error\nmlx_init");
 		ft_exit(all);
 	}
 	if (!(all->mlx.winptr = mlx_new_window(all->mlx.ptr, all->info.res.x, all->info.res.y, "cub3D")))
 	{
-		printf("Error\nmlx_new_window");
+		ft_printf("Error\nmlx_new_window");
 		ft_exit(all);
 	}
 //	ft_textures(all);
 	if (!(all->mlx.imgptr = mlx_new_image(all->mlx.ptr, all->info.res.x, all->info.res.y)))
 	{
-		printf("Error\nmlx_new_image");
+		ft_printf("Error\nmlx_new_image");
 		ft_exit(all);
 	}
 	mlx_string_put(all->mlx.ptr, all->mlx.winptr, 400, 300, all->info.c, "bonjour");
 	//	mlx_put_image_to_window (all->mlx.ptr, all->mlx.winptr,  all->mlx.imgptr, 0, 0);
 	if (!(all->mlx.get_data = (int)mlx_get_data_addr(all->mlx.imgptr, &all->mlx.bits_per_pixel, &all->mlx.size_line, &all->mlx.endian)))
 	{
-		printf("mlx_get_data_addr");
+		ft_printf("mlx_get_data_addr");
 		ft_exit(all);
 	}
 	ft_reycasting(all);
