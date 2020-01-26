2c66ff9b4de564fb685c982d1aea9dce945be44e
Alex 42 <54394628+AlexDos-42@users.noreply.github.com>
2020-01-26 02:59:55 +0100

    Update ft_bonus.c

--- srcs/ft_bonus.c
+++ srcs/ft_bonus.c
@@ -15,8 +15,8 @@ void	ft_hud(t_all *all)
 {
 	if (all->mvt.hud == 1)
 	{
-		mlx_string_put(all->mlx.ptr, all->mlx.winptr, 20, 10, 0xFFFFFF, 
-			"Touches :");
+		mlx_string_put(all->mlx.ptr, all->mlx.winptr, all->info.res.x / 2, all->info.res.x / 2, 0xFFFFFF, 
+			"-- PAUSE --");
 		mlx_string_put(all->mlx.ptr, all->mlx.winptr, 20, 10, 0xFFFFFF, 
 			"W, A, S, D : move");
 		mlx_string_put(all->mlx.ptr, all->mlx.winptr, 20, 30, 0xFFFFFF, 
