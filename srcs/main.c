24dc976f1c290d834dcfc989cc70a7f2c742f8a8
Alex SANTOS <alesanto@e2r13p13.42.fr>
2020-01-28 20:46:54 +0100

    on voit le bout du tunnel

--- srcs/main.c
+++ srcs/main.c
@@ -6,19 +6,25 @@
 /*   By: alesanto <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/11 20:06:07 by alesanto          #+#    #+#             */
-/*   Updated: 2020/01/28 17:46:44 by alesanto         ###   ########.fr       */
+/*   Updated: 2020/01/28 20:46:24 by alesanto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# include "../include/cub3D.h"
+#include "../include/cub3D.h"
 
-void ft_cub(*argv)
+void	ft_cub(char *argv)
 {
-	len = ft_strlen(argv[1]);
-	if (!(ft_strcmp(argv[1] + len - 4, ".cub"))
+	int len;
+
+	len = ft_strlen(argv);
+	if (len - 4 < 0)
+		len = 0;
+	else
+		len = len - 4;
+	if ((ft_strncmp(argv + len, ".cub", 4)))
 	{
 		ft_printf("Invalide map file : file extention isn't .cub");
-		ft_exit(&all);
+		exit(0);
 	}
 }
 
@@ -40,11 +46,11 @@ int		main(int argc, char **argv)
 	}
 	ft_cub(argv[1]);
 	initall(&all, argv);
-	mlx_do_key_autorepeatoff(all.mlx.ptr);	
+	mlx_do_key_autorepeatoff(all.mlx.ptr);
 	mlx_hook(all.mlx.winptr, 2, (1L << 0), &ft_push, &all);
 	mlx_hook(all.mlx.winptr, 3, (1L << 1), &ft_depush, &all);
 	mlx_hook(all.mlx.winptr, 17, 0, &ft_redcross, &all);
-	mlx_loop_hook(all.mlx.ptr, &ft_keys, &all);	
+	mlx_loop_hook(all.mlx.ptr, &ft_keys, &all);
 	mlx_loop(all.mlx.ptr);
 	return (0);
 }
