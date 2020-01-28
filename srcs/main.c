cb567b84a96dba787e5999f944ed7c8adba2c5d2
Alex 42 <54394628+AlexDos-42@users.noreply.github.com>
2020-01-28 20:33:59 +0100

    Update main.c

--- srcs/main.c
+++ srcs/main.c
@@ -12,6 +12,16 @@
 
 # include "../include/cub3D.h"
 
+void ft_cub(*argv)
+{
+	len = ft_strlen(argv[1]);
+	if (!(ft_strcmp(argv[1] + len - 4, ".cub"))
+	{
+		ft_printf("Invalide map file : file extention isn't .cub");
+		ft_exit(&all);
+	}
+}
+
 int		ft_redcross(t_all *all)
 {
 	ft_printf("GAME OVER");
@@ -28,13 +38,13 @@ int		main(int argc, char **argv)
 		ft_printf(ERROR_ARGC, argc);
 		ft_exit(&all);
 	}
+	ft_cub(argv[1]);
 	initall(&all, argv);
 	mlx_do_key_autorepeatoff(all.mlx.ptr);	
 	mlx_hook(all.mlx.winptr, 2, (1L << 0), &ft_push, &all);
 	mlx_hook(all.mlx.winptr, 3, (1L << 1), &ft_depush, &all);
 	mlx_hook(all.mlx.winptr, 17, 0, &ft_redcross, &all);
-	mlx_loop_hook(all.mlx.ptr, &ft_keys, &all);
-//	system("leaks a.out");	
+	mlx_loop_hook(all.mlx.ptr, &ft_keys, &all);	
 	mlx_loop(all.mlx.ptr);
 	return (0);
 }
