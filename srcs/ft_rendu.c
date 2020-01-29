ed4927da33ec487ff9b95f1f220f4940f8549085
Alex 42 <54394628+AlexDos-42@users.noreply.github.com>
2020-01-29 18:55:43 +0100

    Update ft_rendu.c

--- srcs/ft_rendu.c
+++ srcs/ft_rendu.c
@@ -65,30 +65,6 @@ void ft_algo(t_all *all)
 	}
 }
 
-t_texture 	ft_mettretexture(t_all *all)
-{
-	if (all->cam.rayDir.x > 0)
-	{
-		if (all->algo.NSEO)
-		{
-			if (all->cam.rayDir.y < 0)
-				return (all->textureN);
-			return (all->textureS);
-		}
-		return (all->textureE);
-	}
-	else
-	{
-		if (all->algo.NSEO)
-		{
-			if (all->cam.rayDir.y < 0)
-				return (all->textureN);
-			return (all->textureS);
-		}
-		return (all->textureW);
-	}
-}
-
 void	ft_initray(t_all *all, int x)
 {
 	all->algo.colX = 2 * x / (double)all->info.res.x - 1;
@@ -106,30 +82,6 @@ void	ft_initray(t_all *all, int x)
 		all->algo.drawstart = 0;
 	if (all->algo.drawend >= all->info.res.y)
 		all->algo.drawend = all->info.res.y;
-}
-
-void ft_drawall(t_all *all, int x)
-{
-	double wallX; 
-	int	texX;
-	int	texY;
-	t_texture img;	
-
-	img = ft_mettretexture(all);
-	if (all->algo.NSEO == 1)
-		wallX = all->cam.raypos.x + all->algo.pDist * all->cam.rayDir.x;
-	else
-		wallX = all->cam.raypos.y + all->algo.pDist * all->cam.rayDir.y;
-	wallX -= floor(wallX);
-	texX = wallX * (double)img.w;
-	while (all->algo.drawstart < all->algo.drawend)
-	{
-		texY = (all->algo.drawstart - all->info.res.y / 2 +
-				all->algo.hauteurLigne / 2) * img.h / all->algo.hauteurLigne;
-		all->mlx.get_data[x + (int)all->algo.drawstart
-			* (all->mlx.size_line / 4)] = img.data[texX + texY * img.w];
-		all->algo.drawstart++;
-	}
 }
 
 void ft_reycasting(t_all *all)
